223b90d4cd08477cb7ddaac5b96de73b88235973
Larko_0 <larionov1001@gmail.com>
2021-08-13 22:13:02 +0300

    Done with constructor - base version

--- Functions/Procedural/CoursesAdmin/main.py
+++ Functions/Procedural/CoursesAdmin/main.py
@@ -1,6 +1,75 @@
 import datetime
 
 
+clients = [
+    {
+        'name': 'Bob',
+        'surname': 'Bobenko',
+        'balance': 200
+    },
+    {
+        'name': 'Katia',
+        'surname': 'Vasilenko',
+        'balance': 400
+    },
+    {
+        'name': 'Alice',
+        'surname': 'Burenko',
+        'balance': 1500
+    }
+]
+
+courses = [
+    {
+        'name': 'C bases',
+        'description': 'best C course',
+        'teacher_price': 200,
+        'client_price': 300
+    },
+    {
+        'name': 'Python bases',
+        'description': 'best Py course',
+        'teacher_price': 300,
+        'client_price': 400
+    },
+    {
+        'name': 'Math',
+        'description': 'best C course',
+        'teacher_price': 100,
+        'client_price': 200
+    },
+]
+
+teachers = [
+    {
+        'name': 'Teacher',
+        'surname': 'Bober',
+        'courses': [courses[0], courses[1]],
+        'master_koef': 2
+    }
+]
+
+groups = [
+    {
+        'number': 1,
+        'students': [clients[0], clients[1]],
+        'teacher': None,
+        'course': courses[0]
+    }
+]
+
+lessons = [
+    {
+        'date': datetime.date(2021, 7, 30),
+        'time': datetime.time(hour=16),
+        'duration': datetime.timedelta(hours=1.5),
+        'present_students': [],
+        'theme': '',
+        'group': groups[0]
+    }
+]
+
+
 clients_columns = [
     {
         'name': 'name',
@@ -85,7 +154,10 @@ teachers_columns = [
     },
     {
         'name': 'courses',
-        'type': 'str'
+        'type': 'multiple_link',
+        'to_list': courses,
+        'to_columns': courses_columns,
+        'main_field': 'name'
     },
     {
         'name': 'master_koef',
@@ -100,80 +172,25 @@ groups_columns = [
     },
     {
         'name': 'students',
-        'type': 'str'
+        'type': 'multiple_link',
+        'to_list': clients,
+        'to_columns': clients_columns,
+        'main_field': 'surname'
     },
     {
         'name': 'teacher',
-        'type': 'str'
+        'type': 'link',
+        'to_columns': teachers_columns,
+        'to_list': teachers,
+        'main_field': 'surname',
     },
     {
         'name': 'course',
-        'type': 'str'
+        'type': 'link',
+        'to_columns': courses_columns,
+        'to_list': courses,
+        'main_field': 'name'
     },
-]
-
-
-clients = [
-    {
-        'name': 'Bob',
-        'surname': 'Bobenko',
-        'balance': 200
-    },
-    {
-        'name': 'Katia',
-        'surname': 'Vasilenko',
-        'balance': 400
-    }
-]
-
-teachers = [
-    {
-        'name': 'Teacher',
-        'surname': 'Bober',
-        'courses': 'Python, C, math',  # []
-        'master_koef': 2
-    }
-]
-
-courses = [
-    {
-        'name': 'C bases',
-        'description': 'best C course',
-        'teacher_price': 200,
-        'client_price': 300
-    },
-    {
-        'name': 'Python bases',
-        'description': 'best Py course',
-        'teacher_price': 300,
-        'client_price': 400
-    },
-    {
-        'name': 'Math',
-        'description': 'best C course',
-        'teacher_price': 100,
-        'client_price': 200
-    },
-]
-
-groups = [
-    {
-        'number': 1,
-        'students': '[]',  # []
-        'teacher': 'None',  # None
-        'course': 'courses[0]'  # courses[0]
-    }
-]
-
-lessons = [
-    {
-        'date': datetime.date(2021, 7, 30),
-        'time': datetime.time(hour=16),
-        'duration': datetime.timedelta(hours=1.5),
-        'present_students': [],
-        'theme': '',
-        'group': groups[0]
-    }
 ]
 
 
@@ -189,11 +206,79 @@ def input_int(question, error_message='Вы ввели не число. Попр
             print(error_message)
 
 
+def input_link(column):
+    to_list = column['to_list']  # список учителей
+    main_field = column['main_field']   # фамилия
+
+    print(f'Выберите {column["name"]}')
+    n = 1
+    for object in to_list:
+        print(f'{n} - {object[main_field]}')
+        n += 1
+
+    number = input_int('Ваш выбор: ')
+    index = number - 1
+
+    object = to_list[index]
+    return object
+
+
+def input_multiple_link(column, object):  # студенты, группа
+    while True:
+        show_data_table('', object[column['name']], column['to_columns'])
+        print(f"0 - назад\n"
+              f"1 - добавить\n"
+              f"2 - удалить")
+        choice = input('Ваш выбор: ')
+        if choice == '0':
+            return
+        elif choice == '1':
+            not_included_objects = []
+            for sub_object in column['to_list']:
+                if sub_object not in object[column['name']]:
+                    not_included_objects.append(sub_object)
+
+            print('0 - назад')
+            n = 1
+            for sub_object in not_included_objects:
+                print(f'{n} - {sub_object[column["main_field"]]}')
+                n += 1
+
+            index = input_int('Ваш выбор: ') - 1
+            if index == -1:
+                continue
+            if 0 <= index < len(not_included_objects):
+                sub_object = not_included_objects[index]
+                object[column['name']].append(sub_object)
+                print('Добавление успешно')
+            else:
+                print('Такого нету')
+        elif choice == '2':
+            print('0 - назад')
+            n = 1
+            for sub_object in object[column['name']]:
+                print(f'{n} - {sub_object[column["main_field"]]}')
+                n += 1
+
+            index = input_int('Ваш выбор: ') - 1
+            if index == -1:
+                continue
+            if 0 <= index < len(object[column['name']]):
+                sub_object = object[column['name']].pop(index)
+                print('Удаление успешно')
+            else:
+                print('Такого нету')
+
+
 def input_column(column):
     if column['type'] == 'str':
         value = input(f'Введите {column["name"]}: ')
     elif column['type'] == 'int':
         value = input_int(f'Введите {column["name"]}: ')
+    elif column['type'] == 'link':
+        value = input_link(column)
+    elif column['type'] == 'multiple_link':
+        pass
     return value
 
 
@@ -213,27 +298,43 @@ def edit_object_menu(object_name, object, columns, data_list):  # 'Client', {'na
         index = choice - 1
         column = columns[index]
 
-        value = input_column(column)
-        object[column['name']] = value
+        if column['type'] != 'multiple_link':
+            value = input_column(column)
+            object[column['name']] = value
+        else:
+            input_multiple_link(column, object)
 
 
 def show_data_table(object_name, data_list, columns):
     sample = '|'
     columns_names = []
     for column in columns:
-        sample += '%-15s|'
-        columns_names.append(column['name'])
+        if column['type'] != 'multiple_link':
+            sample += '%-15s|'
+            columns_names.append(column['name'])
     headers = sample % tuple(columns_names)
     print(headers)
-    print('-' * (16 * len(columns) + 1))
+    print('-' * (16 * len(columns_names) + 1))
 
     for dct in data_list:  # {'name': 'Bob','surname': 'Bobenko','balance': 500}
-        row_text = sample % tuple(dct.values())
+        values = []
+        for column in columns:
+            if column['type'] == 'multiple_link':
+                pass
+            elif column['type'] == 'link':
+                sub_object = dct[column['name']]
+                if sub_object:
+                    values.append(sub_object[column['main_field']])
+                else:
+                    values.append('---')
+            else:
+                values.append(dct[column['name']])
+        row_text = sample % tuple(values)
         print(row_text)
-    print('-' * (16 * len(columns) + 1))
+    print('-' * (16 * len(columns_names) + 1))
 
 
-def work_with_objects_constructor_menu(object_name, data_list, columns):
+def work_with_objects_constructor_menu(object_name, data_list, columns, main_column_name):
     while True:
         print(f'---= {object_name} =---')
         print('0 - назад\n'
@@ -249,7 +350,7 @@ def work_with_objects_constructor_menu(object_name, data_list, columns):
             print('0 - назад')
             n = 1
             for object in data_list:
-                print(f'{n} - {object["name"]}')
+                print(f'{n} - {object[main_column_name]}')
                 n += 1
 
             number = int(input('Ваш выбор: '))
@@ -272,13 +373,13 @@ def main_menu(clients, teachers, courses, groups, lessons):
 
         choice = input('Ваш выбор: ')
         if choice == '1':
-            work_with_objects_constructor_menu('client', clients, clients_columns)
+            work_with_objects_constructor_menu('client', clients, clients_columns, 'surname')
         elif choice == '2':
-            work_with_objects_constructor_menu('teacher', teachers, teachers_columns)
+            work_with_objects_constructor_menu('teacher', teachers, teachers_columns, 'surname')
         elif choice == '3':
-            work_with_objects_constructor_menu('course', courses, courses_columns)
+            work_with_objects_constructor_menu('course', courses, courses_columns, 'name')
         elif choice == '4':
-            work_with_objects_constructor_menu('group', groups, groups_columns)
+            work_with_objects_constructor_menu('group', groups, groups_columns, 'number')
         elif choice == '5':
             pass
 
